995be15bf78719a38b9de8a308cd219bc25e6bbf
Aleksandar Todorov <60142028+BoostedPenguin@users.noreply.github.com>
2022-10-02 09:52:47 +0200

    Added react navigation bootstrap

--- portfolio-mobile/App.tsx
+++ portfolio-mobile/App.tsx
@@ -1,14 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NativeBaseProvider, Box } from "native-base";
+import { StyleSheet, View } from 'react-native';
+import { NativeBaseProvider, Box, Text } from "native-base";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { navigationRef } from './helpers'
+
+const Stack = createNativeStackNavigator()
+
+function ExampleComponent() {
+  return (
+    <Box flex={1} alignItems="center" justifyContent="center">
+      <Text>
+        Mobile portfolio bootstrapped
+      </Text>
+    </Box>
+  )
+}
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Box flex={1} alignItems="center" justifyContent="center">Mobile portfolio bootstrapped</Box>
+      <NavigationContainer ref={navigationRef}>
+        <Stack.Navigator >
+          <Stack.Screen name="GameLobby" options={{ headerShown: false }} component={ExampleComponent} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </NativeBaseProvider>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
