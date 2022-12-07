f86c5abce02eab715b396d1210b8fb11fa2e1f85
Rene Roets <rene.roets@outlook.com>
2022-12-07 16:36:51 +0100

    Video Upload

--- components/VideoUploadEvidence.tsx
+++ components/VideoUploadEvidence.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback, useRef } from 'react'
 import { Dimensions, Platform } from 'react-native';
 import { IconButton, Image, Text, View, HStack, Button, Icon, Box, VStack, Pressable, Input } from "native-base";
 import { MaterialIcons } from '@expo/vector-icons'
@@ -9,8 +9,12 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { Entypo } from '@expo/vector-icons';
 import { NavigationProps } from '../types/NavigationProps';
 import * as DocumentPicker from 'expo-document-picker';
-import { Camera, CameraType } from 'expo-camera';
-import { StyleSheet, TouchableOpacity } from 'react-native';
+import { Camera, CameraCapturedPicture, CameraType } from 'expo-camera';
+import { TouchableOpacity } from 'react-native';
+import { configFiles } from '../sdk';
+import { Video, AVPlaybackStatus } from 'expo-av';
+import { StyleSheet  } from 'react-native';
+
 
 
 const styles = StyleSheet.create({
@@ -33,16 +37,66 @@ const styles = StyleSheet.create({
 export default function ImageUploadEvidence({ navigation }: NavigationProps) {
 
     //  camera permissions
+    const [hasAudioPermission, setHasAudioPermission] = useState<boolean | null>(null);
     const [hasCameraPermission, setHasCameraPermission] = useState<boolean | null>(null);
     const [camera, setCamera] = useState<any>(null);
+    const [evidenceName, setEvidenceName] = useState("")
 
-    const [capturedImage, setCapturedImage] = useState()
+    const video = React.useRef(null);
+    const [isRecording, setIsRecording] = useState(false);  
+    // const [status, setStatus] = React.useState({});
+    const [capturedVideo, setCapturedVideo] = useState<any | null>();
 
-    async function takePicture() {
-        const photo: any = await camera.takePictureAsync()
-        console.log(photo)
-        setCapturedImage(photo)
+
+    async function addVideoEvidence() {
+        if (!capturedVideo) return
+
+        let localUri = capturedVideo.uri;
+        // console.log("log" + localUri)
+        let filename = localUri.split('/').pop();
+
+        if (!filename) return
+
+        let match = /\.(\w+)$/.exec(filename);
+        let type = match ? `video/${match[1]}` : `video`;
+
+        let formData = new FormData();
+
+        
+        //@ts-ignore
+        formData.append('files[]', { uri: localUri, name: filename, type });
+        formData.append('evidence_type', 'file');
+        formData.append('name', evidenceName);
+        //formData.append('collection_ids[]', configFiles.portfolioId);
+
+
+        const res = await fetch(`https://portfolio.drieam.app/api/v1/portfolios/${configFiles.portfolioId}/evidence`, {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'content-type': 'multipart/form-data',
+                Authorization: `Bearer ${configFiles.bearerToken}`,
+                'X-CSRF-Token': configFiles.XCSRF
+            },
+        }).catch(ex => console.log(ex))
+
+        console.log(res)
+
+        navigation.navigate("Home")
     }
+
+       
+    async function takeVideo() {
+            setIsRecording(true);
+            const data = await camera.recordAsync({
+            maxDuration: 5,
+            quality: "720p",
+            });
+            console.log(data)
+            setCapturedVideo(data);
+            setIsRecording(false); 
+        }
+
 
     // Screen Ratio and image padding
     const [imagePadding, setImagePadding] = useState(0);
@@ -51,14 +105,16 @@ export default function ImageUploadEvidence({ navigation }: NavigationProps) {
     const screenRatio = height / width;
     const [isRatioSet, setIsRatioSet] = useState(false);
 
-    // on screen  load, ask for permission to use the camera
     useEffect(() => {
-        async function getCameraStatus() {
-            const { status } = await Camera.requestCameraPermissionsAsync();
-            setHasCameraPermission(status == 'granted');
-        }
-        getCameraStatus();
-    }, []);
+        (async () => {
+          const cameraStatus = await Camera.requestCameraPermissionsAsync();
+          setHasCameraPermission(cameraStatus.status === "granted");
+    
+          const audioStatus = await Camera.requestMicrophonePermissionsAsync();
+          setHasAudioPermission(audioStatus.status === "granted");
+        })();
+      }, []);
+
 
     // set the camera ratio and padding.
     // this code assumes a portrait mode screen
@@ -110,6 +166,64 @@ export default function ImageUploadEvidence({ navigation }: NavigationProps) {
             await prepareRatio();
         }
     };
+    
+    if (capturedVideo) {
+        return (
+            <>
+                <View style={{ flex: 1, marginTop: 50, marginHorizontal: 25 }}>
+                    <HStack>
+                        <IconButton
+                            mr={4}
+                            onPress={() => {
+                                navigation.goBack()
+                            }}
+                            style={{
+                                alignSelf: "center",
+                            }}
+                            size="sm"
+                            borderColor="#797979"
+                            variant="outline"
+                            _icon={{
+                                as: AntDesign,
+                                size: "sm",
+                                name: "left",
+                                color: "#000"
+                            }}
+                        />
+
+                        <VStack>
+                            <Text fontSize={"lg"}>Add video upload evidence</Text>
+                        </VStack>
+                    </HStack>
+
+                    <VStack mt={5}>
+                        <Text>Name</Text>
+                        <Input onChangeText={e => setEvidenceName(e)} placeholder='Name your evidence...'></Input>
+
+                        {capturedVideo &&
+                            <Video
+                                ref={video}
+                                style={{
+                                    height: 500,
+                                    marginVertical: 5,
+                                    alignContent: "center",
+                                    justifyContent: "center",
+                                    // resizeMode: "contain"
+                                }}
+                                source={{
+                                    uri: capturedVideo.uri
+                                }}
+                                useNativeControls
+                                // resizeMode="contain"
+                            />
+
+                        }
+                        <Button mt={2} onPress={addVideoEvidence}>Add</Button>
+                    </VStack>
+                </View>
+            </>
+        )
+    }
 
     if (hasCameraPermission === null) {
         return (
@@ -143,7 +257,7 @@ export default function ImageUploadEvidence({ navigation }: NavigationProps) {
                         justifyContent: "flex-end",
                         alignSelf: 'center',
                         alignItems: 'center',
-                    }} onPress={takePicture}>
+                    }} onPress={takeVideo}>
                         <Box style={{
                             width: 75,
                             height: 75,
