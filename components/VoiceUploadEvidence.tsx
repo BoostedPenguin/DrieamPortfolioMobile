2a0cb0c5d8d4e44cc88ba9c432a6266102fc4efa
Domantas <domantas.mauruca@gmail.com>
2022-12-07 15:38:42 +0100

    Add voice memos

--- components/VoiceUploadEvidence.tsx
+++ components/VoiceUploadEvidence.tsx
@@ -1,86 +1,173 @@
-import React, { useState, useEffect } from 'react'
-import { Platform } from 'react-native';
-import { IconButton, Image, Text, View, HStack, Button, Icon, Box, VStack, Pressable, Input } from "native-base";
-import { MaterialIcons } from '@expo/vector-icons'
-import { AntDesign } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
-import { NavigationProp, ParamListBase } from '@react-navigation/native';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { Entypo } from '@expo/vector-icons';
-import { NavigationProps } from '../types/NavigationProps';
-import * as DocumentPicker from 'expo-document-picker';
-import { FontAwesome } from '@expo/vector-icons';
+import React, { useState } from "react";
+import {
+  IconButton,
+  Text,
+  View,
+  HStack,
+  Button,
+  Box,
+  VStack,
+  Pressable,
+  Input,
+} from "native-base";
+import { AntDesign } from "@expo/vector-icons";
+import { NavigationProps } from "../types/NavigationProps";
+import { FontAwesome } from "@expo/vector-icons";
+import { Audio } from "expo-av";
+import { configFiles } from "../sdk";
 
 export default function VoiceUploadEvidence({ navigation }: NavigationProps) {
-    const [file, setFile] = useState<DocumentPicker.DocumentResult>()
+  const [evidenceName, setEvidenceName] = useState("");
 
-    async function GetDocument() {
-        const document = await DocumentPicker.getDocumentAsync()
-        if (document.type == "cancel")
-            return
+  const [recording, setRecording] = useState<Audio.Recording | undefined>(
+    undefined
+  );
 
-        setFile(document)
+  const [recordingUri, setRecordingUri] = useState<string | null>();
+
+  const startRecordingAudio = async () => {
+    await Audio.requestPermissionsAsync();
+
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: true,
+      playsInSilentModeIOS: true,
+    });
+
+    const { recording } = await Audio.Recording.createAsync(
+      Audio.RecordingOptionsPresets.HIGH_QUALITY
+    );
+
+    setRecording(recording);
+  };
+
+  const stopRecordingAudio = async () => {
+    setRecording(undefined);
+
+    if (recording) {
+      await recording.stopAndUnloadAsync();
+
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: false,
+      });
+
+      setRecordingUri(recording.getURI());
+    }
+  };
+
+  const getRecordingText = () => {
+    if (recording !== undefined) {
+      return "Stop Recording";
     }
 
-    // useEffect(() => {
-    //     console.log(file)
-    // }, [file])
+    if (recordingUri === undefined && recording === undefined) {
+      return "Start Recording";
+    }
 
-    return (
-        <>
-            <View style={{ flex: 1, marginTop: 50, marginHorizontal: 25 }}>
-                <HStack>
-                    <IconButton
-                        mr={4}
-                        onPress={() => {
-                            navigation.goBack()
-                        }}
-                        style={{
-                            alignSelf: "center",
-                        }}
-                        size="sm"
-                        borderColor="#797979"
-                        variant="outline"
-                        _icon={{
-                            as: AntDesign,
-                            size: "sm",
-                            name: "left",
-                            color: "#000"
-                        }}
-                    />
+    return "Start New Recording";
+  };
 
-                    <VStack>
-                        <Text fontSize={"lg"}>Add voice upload evidence</Text>
-                    </VStack>
-                </HStack>
+  const onRecordButtonClick = async () => {
+    if (recording) {
+      await stopRecordingAudio();
+    } else {
+      await startRecordingAudio();
+    }
+  };
 
-                <VStack mt={5}>
-                    <Text>Name</Text>
-                    <Input placeholder='Name your evidence...'></Input>
+  const sendEvidence = async () => {
+    let formData = new FormData();
 
-                    <Pressable onPress={GetDocument}>
-                        {({ isHovered, isFocused, isPressed }) => {
-                            return (
-                                <Box my={4} alignItems={"center"} justifyContent={"center"} borderColor="red.500" borderWidth={1} height={70} p={3} bg={isPressed ? "#DADADA" : "#F2F2F2"} style={{
-                                    elevation: 1
-                                }}>
-                                    <FontAwesome name="microphone" size={24} color="black" />
-                                    <Text>Click here to capture audio</Text>
-                                </Box>
-                            )
-                        }}
-                    </Pressable>
-                    {file && file.type == "success" &&
-                        <Box borderWidth={1} p={2} borderColor="#797979">
-                            <HStack>
-                                <MaterialIcons name="attach-file" size={24} color="black" />
-                                <Text>{file.name}</Text>
-                            </HStack>
-                        </Box>
-                    }
-                    <Button mt={2}>Add</Button>
-                </VStack>
-            </View>
-        </>
-    )
+    formData.append("files[]", {
+      //@ts-ignore
+      uri: recordingUri,
+      name: evidenceName,
+      type: "audio",
+    });
+    formData.append("evidence_type", "file");
+    formData.append("name", evidenceName);
+
+    const result = await fetch(
+      `https://portfolio.drieam.app/api/v1/portfolios/${configFiles.portfolioId}/evidence`,
+      {
+        method: "POST",
+        body: formData,
+        headers: {
+          "content-type": "multipart/form-data",
+          Authorization: `Bearer ${configFiles.bearerToken}`,
+          "X-CSRF-Token": configFiles.XCSRF,
+        },
+      }
+    ).catch((error) => console.log(error));
+
+    console.log(result);
+
+    navigation.navigate("Home");
+  };
+
+  return (
+    <>
+      <View style={{ flex: 1, marginTop: 50, marginHorizontal: 25 }}>
+        <HStack>
+          <IconButton
+            mr={4}
+            onPress={() => {
+              navigation.goBack();
+            }}
+            style={{
+              alignSelf: "center",
+            }}
+            size="sm"
+            borderColor="#797979"
+            variant="outline"
+            _icon={{
+              as: AntDesign,
+              size: "sm",
+              name: "left",
+              color: "#000",
+            }}
+          />
+
+          <VStack>
+            <Text fontSize={"lg"}>Add voice upload evidence</Text>
+          </VStack>
+        </HStack>
+
+        <VStack mt={5}>
+          <Text>Name</Text>
+          <Input
+            placeholder="Name your evidence..."
+            onChangeText={(value) => setEvidenceName(value)}
+          />
+
+          <Pressable onPress={async () => await onRecordButtonClick()}>
+            {({ isPressed }) => {
+              return (
+                <Box
+                  my={4}
+                  alignItems={"center"}
+                  justifyContent={"center"}
+                  borderColor="red.500"
+                  borderWidth={1}
+                  height={70}
+                  p={3}
+                  bg={isPressed ? "#DADADA" : "#F2F2F2"}
+                  style={{
+                    elevation: 1,
+                  }}
+                >
+                  <FontAwesome name="microphone" size={24} color="black" />
+                  <Text>{getRecordingText()}</Text>
+                </Box>
+              );
+            }}
+          </Pressable>
+          {recordingUri && (
+            <Button mt={2} onPress={async () => await sendEvidence()}>
+              Upload recording
+            </Button>
+          )}
+        </VStack>
+      </View>
+    </>
+  );
 }
